4be5d59c05572ebf9f9fcbab83cc833c4394b531
Susan Cheng <susan.doggie@gmail.com>
2026-02-12 10:46:30 +0800

    Update index.tsx

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -90,7 +90,7 @@ export const Route = ({
 export const Outlet = () => {
   const parent = useContext(Context);
   return (
-    <Context value={{ path: parent.path }}>
+    <Context value={{ path: parent.path, params: parent.params }}>
       {parent.outlet}
     </Context>
   );
